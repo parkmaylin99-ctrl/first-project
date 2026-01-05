0d7f40dfe69b2273a6cc77cdc2638484bb5c1ce8
meka <parkmaylin99@gmail.com>
2026-01-05 19:03:03 +0600

    Add tab functionality with auto-switching feature

--- js/lesson.js
+++ js/lesson.js
@@ -13,3 +13,57 @@ phoneBtn.addEventListener('click', () => {
     phoneResult.innerHTML = 'invalid'
   }
 })
+
+
+const tabBlocks = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+let intervalId;
+
+const hideBlocks = () => {
+  tabBlocks.forEach((item) => {
+    item.style.display = 'none'
+  });
+  tabs.forEach((item) => {
+    item.classList.remove('tab_content_item_active')
+  })
+};
+
+const showBlock = (index = 0) => {
+  tabBlocks[index].style.display = 'block'
+  tabs[index].classList.add('tab_content_item_active') 
+};
+
+hideBlocks();
+showBlock()
+
+tabsParent.addEventListener('click', (event) => {
+  if(event.target.tagName.toLowerCase() === 'button'){
+    tabs.forEach((item, index) => {
+      if(event.target === item){
+        clearInterval(intervalId);
+        hideBlocks();
+        showBlock(index);
+        intervalId = setInterval(() => {
+          index++;
+          if(index > tabs.length - 1){
+            index = 0;
+          }
+          hideBlocks();
+          showBlock(index);
+        }, 3000)
+      }
+    })
+  }
+})
+
+let index = 0;
+
+intervalId = setInterval(() => {
+          index++;
+          if(index > tabs.length - 1){
+            index = 0;
+          }
+        hideBlocks()
+        showBlock(index);
+        }, 3000)
